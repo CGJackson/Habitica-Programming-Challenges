1e4d1e57cc2493adeca4e491fa9b3bcf41ce6190
Conor Jackson <conorgjackson@gmail.com>
2018-11-16 17:30:52 +0000

    wrote symbol and word level parsers, currently untested

--- Nov2018-WordSums/InputParser.hs
+++ Nov2018-WordSums/InputParser.hs
@@ -5,6 +5,7 @@ module InputParser
 ( parser
 ) where
 
+import Control.Monad (foldM)
 import qualified Data.Map as Map
 import qualified Data.Char as Char
 import Data.Boolean (xor)
@@ -19,24 +20,16 @@ data SymbolData = SymbolData{coefficent=SymbolValue, leadingSymbol=Bool} derivin
 (+):: SymbolData -> SymbolData -> SymbolData
 (+) SymbolData{coefficent=coef1,leadingSymbol=leading1} SymbolData{coefficent=coef2,leadingSymbol=leading2} = SymbolData{coefficent=(coef1+coef2),leadingSymbol=(leading1 `or` leading2)
 
-negate:: SymbolData -> SymbolData
-negate SymbolData{coefficent=coef,constantTotal=tot} = SymbolData{coefficent=(-coef), constantTotal=tot}
-
-numberBase:: SymbolValue
-numberBase = 10
-
-digitShift:: SymbolData -SymbolData
-digitShift SymbolData{coefficent=coef, leadingSymbol=l} = SymbolData{coefficent=(numberBase*coef), leadingSymbol=l}
-
 -- Stores data about the input string at in intermediary stage of processing the input
 data InputStringData = InputStringData{symbolData=(Map.Map Char SymbolData), constantTotal=SymbolValue} deriving Eq
 
 -- maps a function over the symbol coefficents of an InputStringData
 mapC:: (SymbolValue -> SymbolValue) -> InputStringData -> InputStringData
-mapC
+mapC f InputStringData{symbolData=symbols, constantTotal=c} = InputStringData{symbolData=(map (\(x,l)->(f x,l)) symbols), constantTotal=c}
 
 -- maps a function over the symbol coefficents and constantTotal of an InputStringData
-mapO:: (SymbolValue -> SymbolValue) -> InputStringData -> InputStringData
+mapCO:: (SymbolValue -> SymbolValue) -> InputStringData -> InputStringData
+mapCO f InputStringData{symbolData=symbols, constantTotal=c} = InputStringData{symbolData=(map (\(x,l)->(f x,l)) symbols), constantTotal=(f c)}
 
 -- Combines the data from 2 'independant' parts of the input, for example 2 different words,
 (+):: InputStringData -> InputStringData -> InputStringData
@@ -45,17 +38,32 @@ mapO:: (SymbolValue -> SymbolValue) -> InputStringData -> InputStringData
 
 -- Changes the sign on coefficents and constant in InputStringData object
 negate:: InputStringData -> InputStringData
-negateInputStringData{symbolData=symbols, constantTotal=offset} = InputStringData{symbolData=newSymbols, constantTotal=(-offset)}
-    where newSymbols = map negate symbols
+negate = mapCO negate
 
-negateIf:: Bool -> InputStringData
+-- Scales the data in an InputStringData so that it referes to refers to a more significant digit in a decimal number,
+-- so 1 -> 10, 43 -> 430, etc. 
+shiftDigitsLeft:: InputStringData -> InputStringData
+shiftDigitsLeft = mapCO (*numberBase)
+    where numberBase = 10
+
+negateIf:: Bool -> InputStringData -> InputStringData
 negateIf True = negate
 negateIf False = id
 
-parseNonLeadingSymbol:: Char -> InputStringData ->  Either ErrorMessage InputStringData
-parseNonLeadingSymbol c InputStringData{symbols=symbols', constantTotal=offset}
-    | Char.isdigit c = InputStringData{symbols=symbols', constantTotal=(offset+(Char.digitToInt c))}
-    | Char.isLetter c = InputStringData{symbols=(Map.insertWith (+) 
+-- Processes the data for a single character in a word and adds it to the data for earlier characters in the word. 
+-- Shifts the data for previous characters to account for the fact that the represent more significant digits.
+parseSymbol:: Bool -> InputStringData -> Char -> Either ErrorMessage InputStringData
+parseSymbol isLeading previousSymbols c
+    | Char.isdigit c = Right InputStringData{symbolData=symbols', constantTotal=(offset+(Char.digitToInt c))}
+    | Char.isLetter c = Right InputStringData{symbolData=(Map.insertWith (+) c (1,isLeading) symbols', constantTotal=offset}
+    | otherwise = Left "Invalid character " ++ [c]
+    where InputStringData{symbols=symbols', constantTotal=offset} = shiftDigitsLeft previousSymbols
 
+-- Processes a string containing a single word and returns the contribution of that
+-- word to the sum as an InputStringData object. If the string contains invalid 
+-- characters returns an error message. 
 parseWord:: String -> Either ErrorMessage InputStringData
-
+parseWord [] = Right InputStringData{symbolData=Map.empty, constantTotal=0}
+parseWord (leadingC:remainingCs) = foldM parseNonLeadingCharacter leadingCharacterData remainingCs
+    where leadingCharacterData = parseSymbol True InputStringData{symbolData=Map.empty, constantTotal=0} leadingC 
+          parseNonLeadingCharacter = parseSymbol False
