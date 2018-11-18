f937250544b71d14a50c895751ced5e8d536ce54
Conor Jackson <conorgjackson@gmail.com>
2018-11-18 01:38:34 +0000

    Wrote and tested result output functions

--- Nov2018-WordSums/WordSumSolverMain.hs
+++ Nov2018-WordSums/WordSumSolverMain.hs
@@ -1,4 +1,18 @@
 
-
+import Utils (SymbolValue)
 import Solver (solve)
 import InputParser (parser)
+
+formatSolution:: [Char] -> [SymbolValue] -> String
+formatSolution symbolKey solution = (concat symbolResults) ++ "\n"
+    where symbolResults = map (\(s,v)->s:" = " ++ (show v) ++ "\n" )(zip symbolKey solution)
+
+formatResults:: [Char] -> [[SymbolValue]] -> String
+formatResults symbolKey = concat . (map (formatSolution symbolKey)) 
+
+printResult:: [Char] -> [[SymbolValue]] -> IO ()
+printResult symbolKey = putStrLn .(formatResults symbolKey) 
+
+main = do putStrLn "Enter a word sum problem:"
+          
+
