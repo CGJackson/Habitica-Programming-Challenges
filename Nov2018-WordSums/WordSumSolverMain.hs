7c08bf943ea5777ec0cce8d482ce156f3d9da411
Conor Jackson <conorgjackson@gmail.com>
2018-11-19 14:54:08 +0000

    Completed Word Sum progam, but realised I had misunderstood the spesifications. Each letter should denote a unique character

--- Nov2018-WordSums/WordSumSolverMain.hs
+++ Nov2018-WordSums/WordSumSolverMain.hs
@@ -1,11 +1,12 @@
 
+import Data.Char (toLower)
 import Utils (SymbolValue, ErrorMessage, WordSumProblem)
 import Solver (solve)
 import InputParser (parser)
 
 formatSolution:: [Char] -> [SymbolValue] -> String
 formatSolution symbolKey solution = (concat symbolResults) ++ "\n"
-    where symbolResults = map (\(s,v)->s:" = " ++ (show v) ++ "\n" )(zip symbolKey solution)
+    where symbolResults = map (\(s,v)->s:" = " ++ (show v) ++ " " )(zip symbolKey solution)
 
 formatResults:: [Char] -> [[SymbolValue]] -> String
 formatResults symbolKey = concat . (map (formatSolution symbolKey)) 
@@ -18,11 +19,20 @@ invalidInput message = do putStrLn ("Invalid input: " ++ message)
                           main
 
 solveAndPrint:: (WordSumProblem,[Char]) -> IO ()
-solveAndPrint (problem, symbolKey) = (printResult symbolKey).solve problem
+solveAndPrint (problem, symbolKey) = (printResult symbolKey) (solve problem)
+
+repeatProgram:: Char -> IO ()
+repeatProgram 'y' = do getLine -- Clears input line befor restarting
+                       main
+repeatProgram 'n' = return ()
+repeatProgram _ = do putStrLn "Invalid input: please enter y or n"
+                     getLine
+                     repeatProgram . toLower =<< getChar
 
 main = do putStrLn "Enter a word sum problem:"
-          input <- (getLine >>= parser)
-          either invalidInput solveAndPrint input
+          ((either invalidInput solveAndPrint).parser) =<< getLine
+          putStrLn "Would you like to enter another word sum problem? [y/n]"
+          repeatProgram . toLower =<< getChar
           
           
 
